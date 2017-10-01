3f6f9ed4146c6579b2ae693401b8720d05d82120
reymesson1 <rey_messon@hotmail.com>
2017-09-30 20:01:37 -0400

    Login Solved

--- webPaa830/app.js
+++ webPaa830/app.js
@@ -1,7 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var app = express();
 
 app.use(express.static('static'))
+
+app.use(bodyParser.json());
+
+var cookies = false;
+
+app.get('/cookies', function(req,res){
+    
+    res.send(cookies);
+});
+
+app.post('/cookies', function(req,res){
+    
+    var newCookie = req.body;
+    
+    if(newCookie.username=='admin'){
+        console.log(req.body);
+        cookies=true;       
+    }
+})
 
 app.listen(80);
