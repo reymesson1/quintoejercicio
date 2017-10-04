4ffab4fcc863eb7323ed3144720b3ba0b67b037b
reymesson1 <rey_messon@hotmail.com>
2017-10-04 02:47:27 +0200

    app logout

--- webPaa830/app.js
+++ webPaa830/app.js
@@ -10,18 +10,24 @@ app.use(bodyParser.json());
 var cookies = false;
 
 app.get('/cookies', function(req,res){
-    
+
     res.send(cookies);
 });
 
 app.post('/cookies', function(req,res){
-    
+
     var newCookie = req.body;
-    
+
     if(newCookie.username=='admin'){
         console.log(req.body);
-        cookies=true;       
+        cookies=true;
     }
-})
+});
 
-app.listen(8080);
+app.get('/logout',function(req,res){
+
+    cookies = false;
+    res.redirect('/');
+});
+
+app.listen(80);
